2df05d9d11ed115d72ac9c0e1ac3023232a03dc2
RealCatDev <real.catdev@gmail.com>
2024-11-01 23:58:09 +0100

    Very cool changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,17 @@
-﻿
-#include "raylib.h"
-
-#include "config.h"
+﻿#include "raylib.h"
 
 int main() {
-    InitWindow(Game::ScreenWidth, Game::ScreenHeight, Game::PROJECT_NAME);
+    InitWindow(800, 600, "GameOff2024");
     SetTargetFPS(60);
 
 
     Texture2D myTexture = LoadTexture("assets/graphics/testimage.png");
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
         BeginDrawing();
             ClearBackground(Color{0,255,0});
             DrawText("Hello, world!", 10, 10, 30, LIGHTGRAY);
             DrawTexture(myTexture, 10, 100, WHITE);
-
         EndDrawing();
     } 
 
